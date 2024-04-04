bfc40c8c75e15b5cdaf576755172b3f91d572df7
misato333 <153616432+misato333@users.noreply.github.com>
2024-04-05 01:26:41 +0900

    portfolio修正6

--- portfolio_honda/js/script.js
+++ portfolio_honda/js/script.js
@@ -1,8 +1,44 @@
-(function(d) {
+(function (d) {
     var config = {
-      kitId: 'qcm6xmp',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
+            kitId: "qcm6xmp",
+            scriptTimeout: 3000,
+            async: true,
+        },
+        h = d.documentElement,
+        t = setTimeout(function () {
+            h.className =
+                h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+    h.className += " wf-loading";
+    tk.src = "https://use.typekit.net/" + config.kitId + ".js";
+    tk.async = true;
+    tk.onload = tk.onreadystatechange = function () {
+        a = this.readyState;
+        if (f || (a && a != "complete" && a != "loaded")) return;
+        f = true;
+        clearTimeout(t);
+        try {
+            Typekit.load(config);
+        } catch (e) {}
+    };
+    s.parentNode.insertBefore(tk, s);
+})(document);
+
+$(function () {
+    $(".sp_btn, .sp_nav li").on("click", function () {
+        $(".sp_nav").fadeToggle();
+        $(".sp_btn").toggleClass("open");
+    });
+});
+
+$(window).on("loading", function () {
+    $(".loading").delay(500).fadeOut(500);
+    $(".loading").delay(800).fadeOut(700);
+});
+setTimeout(function () {
+    $(".loading").fadeOut(300);
+}, 3000);
